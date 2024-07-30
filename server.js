afd4634b33764b0a3a33b63b0f85c66ad1f64f32
trxngxx <nnartnav@gmail.com>
2024-07-30 17:09:29 +0700

    update logging connect to database

--- server.js
+++ server.js
@@ -32,6 +32,23 @@ pool.connect(err => {
   }
 });
 
+// Check connection to PostgreSQL
+pool.connect((err, client, release) => {
+    if (err) {
+      console.error('Error connecting to PostgreSQL:', err.stack);
+      console.error('Connection details:', {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+      });
+    } else {
+      console.log('Connected to PostgreSQL');
+      release();
+    }
+  });
+
 // CartService implementation
 const cartService = {
   AddItem: async (call, callback) => {
